5f3b6d669f9b4bfdb3f071f667fb7a4cfc59a5ad
omka <mcscare12@gmail.com>
2023-09-15 02:10:25 +0300

    Add Redis caching + some fixes

--- vkbot/main.py
+++ vkbot/main.py
@@ -4,13 +4,10 @@ import time
 import redis
 import requests
 
-from dotenv import load_dotenv, find_dotenv
 from vk_api import vk_api
 from vk_api.keyboard import VkKeyboard
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from vk_api.utils import get_random_id
-
-load_dotenv(find_dotenv())
 
 
 def build_one_button(_entrance: str, _entrances: list) -> dict:
@@ -53,15 +50,20 @@ while True:
 
         for event in long_poll.listen():
             if event.type == VkBotEventType.MESSAGE_NEW:
+
+                if not vk.groups.isMember(group_id=os.getenv('VK_GROUP_ID'), user_id=event.obj.message['from_id']):
+                    continue
+
                 if event.obj.message['text'].lower() == 'начать':
                     vk.messages.send(
                         user_id=event.obj.message['from_id'],
                         random_id=get_random_id(),
-                        message='Введи город (не обязательно), улицу и номер дома или отправь геолокацию',
+                        message='Введи город (не обязательно), улицу и номер дома или отправь геолокацию. '
+                                'Перед этим нужно подписаться на группу.',
                         keyboard=keyboard.get_keyboard()
                     )
-                    raise requests.exceptions.ReadTimeout
-                response = None
+
+                response = ''
                 if 'geo' in event.obj.message:
                     lat, lon = event.obj.message['geo']['coordinates'].values()
                     response = requests.get(
@@ -90,8 +92,8 @@ while True:
                 for i in range(number_of_messages):
                     ent_slice = entrances[i * 10: (i + 1) * 10]
                     buttons = build_many_buttons(ent_slice)
-                    if not r.get(f"vk:{str(event.obj.user_id)}"):
-                        vk.messages.send(
+                    if not r.get(f"vk:user:{event.obj.message['from_id']}:action"):
+                        msg_id = vk.messages.send(
                             user_id=event.obj.message['from_id'],
                             random_id=get_random_id(),
                             message=result['address'],
@@ -100,15 +102,19 @@ while True:
                                 "buttons": buttons
                             })
                         )
-                        r.set(f'vk:{event.obj.user_id}', 'button_tap', ex=5)
+                        conversation_message_id = vk.messages.getById(
+                            message_ids=msg_id
+                        )['items'][0]['conversation_message_id']
+                        r.set(f"vk:user:{event.obj.message['from_id']}:message:{conversation_message_id}",
+                              value=str(result).replace("\'", "\""), ex=300)
 
             elif event.type == VkBotEventType.MESSAGE_EVENT:
 
                 payload = event.obj['payload']
                 buttons = build_many_buttons(payload['ent_slice'])
 
-                if r.get(f"vk:{str(event.obj.user_id)}"):
-                    n = r.ttl(f"vk:{str(event.obj.user_id)}") + 1
+                if r.get(f"vk:user:{event.obj.user_id}:action"):
+                    n = r.ttl(f"vk:user:{event.obj.user_id}:action") + 1
                     sec_str = 'секунд' + {n % 10 == 1 and n % 100 != 11: 'у',
                                           n % 10 in [2, 3, 4] and n not in range(10, 15): 'ы'}.setdefault(1, '')
                     vk.messages.sendMessageEventAnswer(
@@ -121,19 +127,20 @@ while True:
                         })
                     )
                 else:
-
-                    msg = vk.messages.getByConversationMessageId(
-                        peer_id=event.obj.peer_id,
-                        conversation_message_ids=event.obj['conversation_message_id'],
-                    )
-
-                    address = msg['items'][0]['text'].split('\n')[0]
-                    response = requests.get(
-                        f"http://{os.getenv('DOMOFOMKA_API_HOST')}:{os.getenv('DOMOFOMKA_API_PORT')}"
-                        f"/codes_msg?message={address.replace(',', '').replace(' дом ', ' ')}"
-                    )
-                    print(response)
-                    result = response.json()
+                    response = r.get(f"vk:user:{str(event.obj.user_id)}:message:{event.obj['conversation_message_id']}")
+                    if not response:
+                        msg = vk.messages.getByConversationMessageId(
+                            peer_id=event.obj.peer_id,
+                            conversation_message_ids=event.obj['conversation_message_id'],
+                        )
+                        address = msg['items'][0]['text'].split('\n')[0]
+                        response = requests.get(
+                            f"http://{os.getenv('DOMOFOMKA_API_HOST')}:{os.getenv('DOMOFOMKA_API_PORT')}"
+                            f"/codes_msg?message={address.replace(',', '').replace(' дом ', ' ')}"
+                        )
+                        result = response.json()
+                    else:
+                        result = json.loads(str(response))
 
                     possible_types = ['yaeda', 'delivery', 'oldcodes']
                     codes = result['data'][payload['entrance']]
@@ -148,13 +155,16 @@ while True:
                     vk.messages.edit(
                         peer_id=event.obj.peer_id,
                         conversation_message_id=event.obj['conversation_message_id'],
-                        message=f"{address}\nПодъезд {payload['entrance']}\n{answer}",
+                        message=f"{result['address']}\nПодъезд {payload['entrance']}\n{answer}",
                         keyboard=json.dumps({
                             "inline": True,
                             "buttons": buttons
                         })
                     )
-                    r.set(f'vk:{event.obj.user_id}', 'button_tap', ex=9)
+
+                    r.set(f'vk:user:{event.obj.user_id}:action', 'button_tap', ex=9)
+                    r.set(f"vk:user:{str(event.obj.user_id)}:message:{event.obj['conversation_message_id']}",
+                          value=str(result).replace("\'", "\""), ex=300)
 
     except requests.exceptions.ReadTimeout:
         time.sleep(3)
