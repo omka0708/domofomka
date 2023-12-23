02931e64f4f669432bac23767527976bd3d1c856
omka <mcscare12@gmail.com>
2023-12-24 02:23:47 +0300

    Make API 10x+ faster + async

--- api/app/main.py
+++ api/app/main.py
@@ -1,87 +1,23 @@
-import sqlite3
-import os
-import re
-
 from fastapi import FastAPI, Query
+from fastapi.responses import RedirectResponse
 from typing import Annotated
-from dadata import Dadata
 
-app = FastAPI()
-dadata = Dadata(os.getenv('DADATA_TOKEN'))
+from .services import get_data_from_db, get_address_by_geo
 
-
-def address_exists(msg: str, city: str, street: str, house: str, street_type: str) -> bool:
-    msg = (msg.lower().replace('ё', 'е').replace(' дом ', ' ').
-           replace('строение', 'с').replace('корпус', 'к'))
-    msg, city, street, house = map(
-        lambda x: re.sub(r'\W', '', x).lower().replace('ё', 'е'),
-        [msg, city, street, house]
-    )
-    if msg.count(street) == 1:
-        msg = msg.replace(street, '')
-        if msg.count(house) == 1:
-            msg = msg.replace(house, '')
-            if msg:  # msg have something more than street and house
-                if street_type in msg:  # it is probably street type
-                    msg = msg.replace(street_type, '')
-                res = True
-                for remaining_word in msg.split():  # if there is anything left in msg, it must be in city
-                    if remaining_word not in city or len(remaining_word) < 4:
-                        res = False
-                        break
-            else:
-                res = True
-        else:
-            res = False
-    else:
-        res = False
-    return res
+app = FastAPI(title="Domofomka")
 
 
-def get_data_from_db(msg: str) -> dict:
-    res = {}
-    if not msg:
-        return res
-    with sqlite3.connect(os.getenv('DB_NAME')) as connection:
-        cursor = connection.cursor()
-        connection.create_function("address_exists", 5, address_exists)
-        query = f'''
-            select * 
-                from codes 
-                    where address_exists('{msg}', city, street, house, street_type)
-        '''
-        data = cursor.execute(query).fetchall()
-        if data:
-            shortest_city = data[0][1]
-            for _id, city, *args in data:
-                if len(city) < len(shortest_city):
-                    shortest_city = city
-            for _id, city, street_type, street, house, entrance, code_type, code in data:
-                if shortest_city in city:
-                    res.setdefault('address', f"{shortest_city}, {street_type} {street}, дом {house}")
-                    res.setdefault('data', {})
-                    res['data'].setdefault(entrance, []).append((code, code_type))
-    return res
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url='/docs')
 
 
-def get_address_by_geo(lat: float, lon: float) -> str | None:
-    houses = dadata.geolocate(name="address", lat=lat, lon=lon)
-    address = None
-    if houses:
-        house_data = houses[0]['data']
-        if house_data['street']:
-            address = f"{house_data['city']} {house_data['street']} {house_data['house']}"
-            if house_data['block']:
-                address += house_data['block_type'].replace('стр', 'с') + house_data['block'].replace(' стр ', 'с')
-    return address
-
-
-@app.get("/codes_msg")
+@app.get("/codes_msg", tags=["codes"])
 async def get_codes_by_message(message: Annotated[str, Query(min_length=7, max_length=100)]) -> dict:
-    return get_data_from_db(message)
+    return await get_data_from_db(message)
 
 
-@app.get("/codes_geo")
+@app.get("/codes_geo", tags=["codes"])
 async def get_codes_by_geo(lat: float, lon: float) -> dict:
-    address = get_address_by_geo(lat, lon)
-    return get_data_from_db(address)
+    address = await get_address_by_geo(lat, lon)
+    return await get_data_from_db(address)
---
+++ api/app/services/__init__.py
@@ -0,0 +1,1 @@
+from .codes import get_data_from_db, get_address_by_geo
